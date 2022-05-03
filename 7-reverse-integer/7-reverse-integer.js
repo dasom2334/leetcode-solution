1eac4e952b486c2881df6044e11beaf4f86b13c6
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-05-03 13:24:50 +0900

    Time: 106 ms (39.48%), Space: 44.4 MB (22.83%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.js
+++ 7-reverse-integer/7-reverse-integer.js
@@ -3,24 +3,41 @@
  * @return {number}
  */
 var reverse = function(x) {
-    const limit = 2**31
-    let result = '';
+    let limit = (2**31);
     
+    let result = '';
+    // console.log(2**31);
     let num = x; 
     if (x < 0) {
         num = (-1 * x);
         result = '-';
+    } else {
+        limit--;
     }
-    const str = num.toString().split('').reverse().join('');
+    let str = num.toString().split('').reverse().join('');
+    const limitStr = limit.toString();
+    const limitLen = limitStr.length;
+    // console.log(str, limit);
+    // console.log(str.length, limit.toString().length);
+    
+    // console.log(str);
+    if (str.length > limitLen) {
+        return 0;
+    } else if (str.length == limitLen) {
+        let isSame = true;
+        for (const i in limitStr) {
+            // console.log(parseInt(limitStr[i]), parseInt(str[i]))
+            // console.log(parseInt(limitStr[i]) < parseInt(str[i]))
+            if (parseInt(limitStr[i]) < parseInt(str[i])) {
+                return 0;
+            } else if (parseInt(limitStr[i]) > parseInt(str[i])) {
+                isSame = false;
+                break;
+            }
+        }  
+        if (isSame) return 0;
+    }
     
     result += str;
-    
-    result = parseInt(result);
-    
-    if (result > limit || result < -limit) {
-        result = 0;
-    }
-    
-    
-    return result;
+    return parseInt(result);
 };
