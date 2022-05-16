da2e0089ca44b94384de17cdda0425490fb8577a
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-05-17 01:23:33 +0900

    Time: 985 ms (5.69%), Space: 78.8 MB (6.36%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var shortestPathBinaryMatrix = function(grid) {
+    let toVisit = [[0, 0, 1]];
+    const gl = grid.length;    
+    // let min = gl*gl;
+    
+    let visited = new Array(gl).fill(null).map(e => new Array(gl).fill(gl*gl + 1));
+    
+    
+    let isGoal = false;
+    while (toVisit.length > 0) {
+        const current = toVisit.shift();
+        // const strCurrent = [current[0], current[1]].join();
+        // console.log(current[0], current[1], current[2]);
+        if (visited[current[0]][current[1]] > current[2]) {
+            visited[current[0]][current[1]] = current[2];
+            if (grid[current[0]][current[1]] !== 0) continue;
+            if (current[0] == gl - 1 && current[1] == gl - 1) {
+                // min = Math.min(min, current[2]);
+                isGoal = true;
+            }
+            const visitings = [
+                [current[0]-1, current[1]-1, current[2]+1],
+                [current[0]-1, current[1], current[2]+1],
+                [current[0]-1, current[1]+1, current[2]+1],
+                [current[0], current[1]-1, current[2]+1],
+                [current[0], current[1]+1, current[2]+1],
+                [current[0]+1, current[1]-1, current[2]+1],
+                [current[0]+1, current[1], current[2]+1],
+                [current[0]+1, current[1]+1, current[2]+1],
+            ];
+            const filtered = visitings.filter(e => e[0] >= 0 && e[0] < gl && e[1] >= 0 && e[1] < gl && e[2] <= gl*gl);
+            toVisit = [...toVisit, ...filtered];
+            // console.log(toVisit);
+        }
+    }
+    let result = -1;
+    if (isGoal) {
+        result = visited[gl-1][gl-1];
+    }
+    return result;
+};
