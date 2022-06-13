e92839605c9b1b39dd52b09ee0bb8ef88907e1bf
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-06-13 22:09:30 +0900

    Time: 136 ms (8.20%), Space: 48.6 MB (5.19%) - LeetHub

---
+++ 120-triangle/120-triangle.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+    
+    let temp = [triangle[0][0]];
+    
+    
+    for (let k1 = 1; k1 < triangle.length; k1++) {
+        let temp2 = [];
+        for (const k2 in triangle[k1]) {
+            let temp3 = []
+            if (k2 - 1 >= 0) {
+                temp3 = [...temp3, temp[k2-1]];
+            }
+            // console.log(temp, k2);
+            if (k2 < temp.length) {
+                temp3 = [...temp3, temp[k2]];
+            }
+            // console.log(temp, temp3);
+            temp3 = temp3.map(e => parseInt(e) + parseInt(triangle[k1][k2]));
+            temp2.push(Math.min(...temp3));
+            
+        }
+        temp = temp2;
+    }
+    // console.log(temp);
+    return Math.min(...temp.flat());
+};
