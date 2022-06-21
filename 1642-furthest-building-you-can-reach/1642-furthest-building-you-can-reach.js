713d6968a61f46fbcbe1744fbab1ec958973e76c
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-06-21 21:58:51 +0900

    Time: 1480 ms (7.69%), Space: 64.5 MB (7.69%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number[]} heights
+ * @param {number} bricks
+ * @param {number} ladders
+ * @return {number}
+ */
+var furthestBuilding = function(heights, bricks, ladders) {
+    let buildings = heights.map((e, i) => i > 0 ? (heights[i] - heights[i-1]):0);
+    let sum = buildings.filter(e => e > 0).reduce((p, c) => p + c, 0);
+    let ob = buildings
+            .slice()
+            // .filter(e => e > 0)
+            .sort((a, b) => a - b);
+    let ls = ob
+            .splice(ob.length - ladders)
+            .map(e => (e <= 0 ? 0 : e));
+    let lsum = ls.reduce((a, b) => a + b, 0);
+    let last = 0;
+    let next = 0;
+    // console.log(ls);
+    let isLadderOut = -1;
+    while(buildings.length > 0) {
+        // console.log(ob);
+        // console.log(sum, lsum, sum - lsum, last, bricks, buildings.length, ls);
+        
+        
+        if (sum - lsum <= bricks) break;
+        
+        const olast = buildings.pop();
+        
+        last = (olast > 0)? olast:0;
+        sum -= last;
+        isLadderOut = ls.indexOf(olast);
+        if (isLadderOut != -1) {
+            // console.log(isLadderOut);
+            if (ob.length > 0) {
+                next = ob.pop()
+            } else {
+                next = 0;
+            }
+            // console.log(next);
+            ls[isLadderOut] = next;
+            // ls.splice(isLadderOut, 1, next);
+            // ls.push(next);
+            lsum = lsum + next - last;
+        } else {
+            const i = ob.indexOf(olast);
+            if (i != -1) ob.splice(i, 1);
+            // console.log(i);
+        }
+    }
+    return buildings.length - 1;
+};
