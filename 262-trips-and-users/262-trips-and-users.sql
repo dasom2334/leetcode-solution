16063f541bddb44663af9965bcd077c542b85996
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-06-22 22:42:39 +0900

    Time: 568 ms (45.48%), Space: 0B (100.00%) - LeetHub

---
+++ 262-trips-and-users/262-trips-and-users.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT T.request_at AS Day
+    , ROUND(SUM(CASE WHEN T.status != 'completed' then 1 else 0 end) / COUNT(*), 2) AS 'Cancellation Rate'
+    # , SUM(CASE WHEN T.status != 'completed' then 1 else 0 end) , SUM(CASE WHEN T.status = 'completed' then 1 else 0 end)
+    # , COUNT(*)
+FROM TRIPS AS T
+LEFT JOIN USERS AS C ON C.users_id = T.client_id
+LEFT JOIN USERS AS D ON D.users_id = T.driver_id
+WHERE C.banned = 'No'
+    AND D.banned = 'No'
+    AND T.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY T.request_at
