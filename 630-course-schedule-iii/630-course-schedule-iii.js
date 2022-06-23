00cd65248166eb9da21c13b04fff097c513edee6
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-06-23 17:08:04 +0900

    Time: 2031 ms (24.14%), Space: 50.8 MB (93.10%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.js
+++ 630-course-schedule-iii/630-course-schedule-iii.js
@@ -12,8 +12,9 @@ var scheduleCourse = function(courses) {
             day += c[0]
         } else if (taken.length > 0) {
             const lt = taken[taken.length - 1];
+            const td = day - lt[0] + c[0];
             if (lt[0] > c[0]) {
-                day = day - lt[0] + c[0];
+                day = td;
                 taken.pop();
                 pushTaken(taken, c);
             }
