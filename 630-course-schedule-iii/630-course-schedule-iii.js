f09ff1e5fb8a9792169010e29ea5666d225e5ce7
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-06-23 17:05:11 +0900

    Time: 981 ms (31.03%), Space: 50.4 MB (100.00%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[][]} courses
+ * @return {number}
+ */
+var scheduleCourse = function(courses) {
+    let day = 0;
+    let taken = [];
+    const oc = courses.sort((a, b) => a[1] - b[1]);
+    // console.log(oc);
+    for (const c of oc) {
+        if (day + c[0] <= c[1]) {
+            pushTaken(taken, c);
+            day += c[0]
+        } else if (taken.length > 0) {
+            const lt = taken[taken.length - 1];
+            const td = day - lt[0] + c[0];
+            if (lt[0] > c[0] && td <= c[1]) {
+                day = td;
+                taken.pop();
+                pushTaken(taken, c);
+                // taken.push(c);
+            }
+        }
+        // console.log(taken);
+    }
+    // console.log(taken);
+    // console.log(day);
+    return taken.length;
+};
+
+var pushTaken = function (taken, current) {
+    
+    let isPushed = false;
+    for(let i = 0; i < taken.length; i++) {
+        if (current[0] < taken[i][0]) {
+            taken.splice(i, 0, current);
+            isPushed = true;
+            break;
+        }
+    }
+    if (!isPushed) {
+        taken.push(current);   
+    }
+}
