3cfe896beabb541dba20a2a330e630b27d4b9067
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-06-28 21:39:24 +0900

    Time: 301 ms (17.60%), Space: 48.5 MB (44.53%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.js
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.js
@@ -14,13 +14,9 @@ var minDeletions = function(s) {
         }
     }
     let entry = Object.entries(hash).map(e => e[1]);
-    // console.log(entry);
-    // console.log(hash);
     for (let i = 0; i < entry.length; i++) {
         let isFre = entry.indexOf(entry[i]);
         while(![-1, i].includes(isFre) && entry[i] !== 0) {
-            // console.log(isFre, i, [-1, i, 0].includes(isFre), );
-            // console.log(isFre, entry[i], i, entry, result);
             entry[i]--;
             isFre = entry.indexOf(entry[i]);
             result++;
