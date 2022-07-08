4589f176573f3ec84aecb292c3bf1b1864202eb0
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-07-08 21:37:24 +0900

    Time: 451 ms (45.83%), Space: 79.5 MB (29.17%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.js
+++ 1473-paint-house-iii/1473-paint-house-iii.js
@@ -8,11 +8,10 @@
  */
 var minCost = function(houses, cost, m, n, target) {
     let dp = new Array(m+1).fill(null).map(e => new Array(n+1).fill(null).map(e1 => new Array(target+1).fill(0)));
-        for (let k = 1; k <= target; k++) {
     for (let i = 1; i <= m; i++) {
-
         for (let j = 1; j <= n; j++) {
-                if (i < k || houses[i-1] !== 0 && (j !== houses[i-1] || false) ) {
+            for (let k = 1; k <= target; k++) {
+                if (i < k || houses[i-1] !== 0 && j !== houses[i-1]) {
                     dp[i][j][k] = -1;
                     continue;
                 }
@@ -21,7 +20,6 @@ var minCost = function(houses, cost, m, n, target) {
                     before = dp[i-1].filter((e, i) => ![0, j].includes(i)).map(e => e[k-1]).filter(e => e !== -1);
                 }
                 if ((dp[i-1][j][k]) !== -1) before.push(dp[i-1][j][k]);
-                // console.log(before);
                 if (before.length > 0) {
                     dp[i][j][k] = Math.min(...before);
                     if (houses[i-1] == 0) dp[i][j][k] += cost[i-1][j-1];
@@ -30,7 +28,6 @@ var minCost = function(houses, cost, m, n, target) {
                 }
             }
         }
-
         // console.log(dp);
     }
     const results = dp[dp.length-1].map(e => e[target]).filter((e, i) => i !== 0 && e !== -1);
