4a59de249ce7f2040a5d8f1fff39dce4867936db
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-07-12 17:53:17 +0900

    Time: 685 ms (8.30%), Space: 49 MB (5.10%) - LeetHub

---
+++ 16-3sum-closest/16-3sum-closest.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    const ons = nums.map((e, i) => [i, e]).sort((a, b) => a[1] - b[1]);
+    let min = Infinity;
+    let result = nums[0] + nums[1] + nums[2];
+    for(let i = 0; i < ons.length - 2; i++) {
+        let l = i+1;
+        let r = ons.length - 1;
+        let m = Infinity;
+        while (l < r) {
+            const ts = ons[l][1] + ons[r][1] + ons[i][1];
+            if (ts > target) {
+                r--;
+            } else {
+                l++;
+            }
+            const diff = Math.abs(target - ts);
+            if (diff < min) {
+                min = diff;
+                result = ts;
+            }
+            
+        }
+    }
+    return result;
+};
