bde45d1df135517358f02ac53a4b8901af8992d6
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-07-18 22:59:51 +0900

    Time: 374 ms (42.42%), Space: 50.4 MB (48.48%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.js
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.js
@@ -5,39 +5,39 @@
  */
 var numSubmatrixSumTarget = function(matrix, target) {
     let result = 0;
-    let dp = new Array(matrix.length).fill(null).map(e => new Array(matrix[0].length).fill(null));
+    let dp = new Array(matrix.length).fill(null).map(e => new Array(matrix[0].length).fill(0));
     
     for(let i = 0; i < matrix.length; i++) {
+        let sum = 0;
         for(let j = 0; j < matrix[0].length; j++) {
-            dp[i][j] = [matrix[i][j]];
-            for(let k = 0; k < j; k++) {
-                const last = dp[i][k].length - 1;
-                const sum = dp[i][k][last] + matrix[i][j];
-                dp[i][k].push(sum);
-            }
+            sum += matrix[i][j];
+            dp[i][j] = sum;
         }
     }
     // console.log(dp);
-    dp = dp.map(e => e.flat());
+    // dp = dp.map(e => e.flat());
     let hash = new Map();
     // console.log(dp);
     for (let m = 0; m < dp[0].length; m++) {
-        let sum = 0;
-        hash.clear();
-        hash.set(0, 1);
-        for (let n = 0; n < dp.length; n++) {
-            sum += dp[n][m];
-            // if(sum === target) {
-            //     result++;
-            // } 
-            if(hash.has(sum - target)){
-                result += hash.get(sum - target);
-            }
-            
-            if (hash.has(sum)) {
-                hash.set(sum, hash.get(sum)+1);
-            } else {
-                hash.set(sum, 1);
+        for (let m2 = m; m2 >= 0; m2--) {
+            let sum = 0;
+            hash.clear();
+            hash.set(0, 1);
+            for (let n = 0; n < dp.length; n++) {
+                sum += dp[n][m];
+                if(m2 > 0) sum -= dp[n][m2 - 1];
+                // if(sum === target) {
+                //     result++;
+                // } 
+                if(hash.has(sum - target)){
+                    result += hash.get(sum - target);
+                }
+
+                if (hash.has(sum)) {
+                    hash.set(sum, hash.get(sum)+1);
+                } else {
+                    hash.set(sum, 1);
+                }
             }
         }
     }
