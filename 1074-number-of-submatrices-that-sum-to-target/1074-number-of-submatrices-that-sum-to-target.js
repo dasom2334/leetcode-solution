a0fd074510ac67be80569b474aa1e8f5b640a7e6
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-07-18 23:03:30 +0900

    Time: 283 ms (66.67%), Space: 49.5 MB (78.79%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.js
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.js
@@ -7,29 +7,22 @@ var numSubmatrixSumTarget = function(matrix, target) {
     let result = 0;
     let sum = 0;
     let dp = new Array(matrix.length).fill(null).map(e => new Array(matrix[0].length).fill(0));
-    
     for(let i = 0; i < matrix.length; i++) {
         sum = 0;
         for(let j = 0; j < matrix[0].length; j++) {
             sum += matrix[i][j];
-            dp[i][j] = sum;
+            matrix[i][j] = sum;
         }
     }
-    // console.log(dp);
-    // dp = dp.map(e => e.flat());
     let hash = new Map();
-    // console.log(dp);
-    for (let m = 0; m < dp[0].length; m++) {
+    for (let m = 0; m < matrix[0].length; m++) {
         for (let m2 = m; m2 >= 0; m2--) {
             sum = 0;
             hash.clear();
             hash.set(0, 1);
-            for (let n = 0; n < dp.length; n++) {
-                sum += dp[n][m];
-                if(m2 > 0) sum -= dp[n][m2 - 1];
-                // if(sum === target) {
-                //     result++;
-                // } 
+            for (let n = 0; n < matrix.length; n++) {
+                sum += matrix[n][m];
+                if(m2 > 0) sum -= matrix[n][m2 - 1];
                 if(hash.has(sum - target)){
                     result += hash.get(sum - target);
                 }
@@ -42,7 +35,6 @@ var numSubmatrixSumTarget = function(matrix, target) {
             }
         }
     }
-    // console.log(result);
     
     
     return result;
