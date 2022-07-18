c12abd58c4f965943d4f3f1bc51ecfb6e546a2b0
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-07-18 22:33:44 +0900

    Time: 830 ms (27.27%), Space: 73.8 MB (24.24%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.js
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.js
@@ -12,16 +12,16 @@ var numSubmatrixSumTarget = function(matrix, target) {
     for(let i = 0; i < matrix.length; i++) {
         for(let j = 0; j < matrix[0].length; j++) {
             dp[i][j] = [matrix[i][j]];
-            if (matrix[i][j] === target) {
-                result++;
-            }
+            // if (matrix[i][j] === target) {
+            //     // result++;
+            // }
             for(let k = 0; k < j; k++) {
                 const last = dp[i][k].length - 1;
                 const sum = dp[i][k][last] + matrix[i][j];
                 dp[i][k] = [...dp[i][k], sum];
-                if (sum === target) {
-                    result++;
-                }
+                // if (sum === target) {
+                //     // result++;
+                // }
             }
         }
     }
@@ -29,21 +29,29 @@ var numSubmatrixSumTarget = function(matrix, target) {
     // console.log(result);
     dp = dp.map(e => e.flat());
     
+    // console.log(dp);
     for (let m = 0; m < dp[0].length; m++) {
+        let sum = 0;
+        // let count = 0;
+        let hash = new Map();
         for (let n = 0; n < dp.length; n++) {
-            let sum = 0;
-            let count = 0;
-            for (let l = n; l < dp.length; l++) {
-                sum += dp[l][m];
-                count++;
-                if(sum === target && count !== 1) {
-                    result++;
-                }   
+            sum += dp[n][m];
+            // count++;
+            if(sum === target ) {
+                result++;
+            } 
+            if(hash.has(sum - target)){
+                result += hash.get(sum - target);
+            }
+            
+            if (hash.has(sum)) {
+                hash.set(sum, hash.get(sum)+1);
+            } else {
+                hash.set(sum, 1);
             }
         }
     }
     // console.log(result);
-    // console.log(dp);
     
     
     return result;
