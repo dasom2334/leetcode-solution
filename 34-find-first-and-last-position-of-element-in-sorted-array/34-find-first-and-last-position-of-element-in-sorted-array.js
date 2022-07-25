0780387f05030dd19d2b4cb193ae482ae33f43d0
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-07-25 17:04:44 +0900

    Time: 94 ms (40.68%), Space: 42.6 MB (62.36%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+    let result = [-1, -1];
+    
+    let l = 0;
+    let r = nums.length - 1;
+    let [ml, mr] = [l, r];
+    while (l < r && result.includes(-1)) {
+        if (result[0] == -1) {
+            if (nums[l] < target) {
+                ml = l;
+                l += Math.ceil((r-l)/2);
+            } else if (nums[l] == target && nums[l-1] != target) {
+                result[0] = l;
+            } else {
+                l -= Math.ceil((l-ml)/2);
+                
+            }
+        }
+        
+        if (result[1] == -1) {
+            if (nums[r] > target) {
+                mr = r;
+                r -= Math.ceil((r-l)/2);
+            } else if (nums[r] == target && nums[r+1] != target) {
+                result[1] = r;
+            } else {
+                r += Math.ceil((mr - r)/2);
+            }
+        }
+        
+        // console.log(l, r, result);
+    }
+    
+    if (l == r && nums[l] == target) {
+        result = [l, l]; 
+    }
+    
+    return result;
+};
