a39710c509d6a694686746d87cd97b31451ffcc3
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-07-26 20:22:36 +0900

    Time: 128 ms (41.20%), Space: 51.9 MB (49.83%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -12,6 +12,23 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
+    let flag = false;
+    const dfs = (node) => {
+        if (flag) return;
+        if (!node || node == p || node == q ) {
+            return node;
+        }        
+        const l = dfs(node.left);
+        const r = dfs(node.right);
+        if (l && r) {
+            flag = true;
+            return node;
+        } 
+        return l || r;
+    }
+    return dfs(root);
+};
+var lowestCommonAncestorOld = function(root, p, q) {
     let result = -1;
     
     let stack = [];
