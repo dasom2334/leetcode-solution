5f06692b11cab00f284d750b9494e8a079d927f4
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-07-26 20:25:23 +0900

    Time: 82 ms (93.35%), Space: 51.6 MB (76.59%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -29,58 +29,58 @@ var lowestCommonAncestor = function(root, p, q) {
     return dfs(root);
 };
 
-var lowestCommonAncestorOld = function(root, p, q) {
-    let result = -1;
+// var lowestCommonAncestorOld = function(root, p, q) {
+//     let result = -1;
     
-    let stack = [];
-    const dfs = (node) => {
-        stack.push(node);
+//     let stack = [];
+//     const dfs = (node) => {
+//         stack.push(node);
 
-        if (node == p || node == q) {
-            if (result === -1) {
-                result = stack.length - 1;   
-            } else {
-                return true;
-            }
-        }        
+//         if (node == p || node == q) {
+//             if (result === -1) {
+//                 result = stack.length - 1;   
+//             } else {
+//                 return true;
+//             }
+//         }        
         
-        if ((node.left)?dfs(node.left, stack):false) {
-            return true;
-        }
-        if ((node.right)?dfs(node.right, stack):false) {
-            return true;
-        }
-        stack.pop();
-        if (stack.length < result + 1) {
-            result = stack.length - 1;
-        }
+//         if ((node.left)?dfs(node.left, stack):false) {
+//             return true;
+//         }
+//         if ((node.right)?dfs(node.right, stack):false) {
+//             return true;
+//         }
+//         stack.pop();
+//         if (stack.length < result + 1) {
+//             result = stack.length - 1;
+//         }
         
-    }
-    dfs(root);
-    return stack[result];
-};
+//     }
+//     dfs(root);
+//     return stack[result];
+// };
 
 
-var lowestCommonAncestorOldest = function(root, p, q) {
-    let toVisit = [[root, ""]];
-    let [pi, qi] = [-1, -1];
-    while((pi === -1 || qi === -1)) {
-        const n = toVisit.pop();
-        if (n[0]) {
-            toVisit = [...toVisit, [n[0].left, n[1] + "l"], [n[0].right, n[1] + "r"]];
-            if (n[0] == p) pi = n[1];
-            if (n[0] == q) qi = n[1];
-        }
-    }
-    let index = 0;
-    let result = root;
-    while (pi[index] == qi[index]) {
-        if (pi[index] == "l") {
-            result = result.left;
-        } else {
-            result = result.right;
-        }
-        index++;
-    }
-    return result;
-};
+// var lowestCommonAncestorOldest = function(root, p, q) {
+//     let toVisit = [[root, ""]];
+//     let [pi, qi] = [-1, -1];
+//     while((pi === -1 || qi === -1)) {
+//         const n = toVisit.pop();
+//         if (n[0]) {
+//             toVisit = [...toVisit, [n[0].left, n[1] + "l"], [n[0].right, n[1] + "r"]];
+//             if (n[0] == p) pi = n[1];
+//             if (n[0] == q) qi = n[1];
+//         }
+//     }
+//     let index = 0;
+//     let result = root;
+//     while (pi[index] == qi[index]) {
+//         if (pi[index] == "l") {
+//             result = result.left;
+//         } else {
+//             result = result.right;
+//         }
+//         index++;
+//     }
+//     return result;
+// };
