398bccc76917def32cdedc8ebb54ba9a2355886d
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-07-28 15:48:18 +0900

    Time: 90 ms (87.42%), Space: 42.5 MB (93.09%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,6 +4,18 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    let hash = new Map();
+    for (let i = 0; i < t.length; i++) {
+        const start = hash.get(s[i]) || 0;
+        let index = t.indexOf(s[i], start);
+        hash.set(s[i], index + 1);
+        // console.log(s[i], start, index);
+        if (index == -1) return false;
+    }
+    return true;
+};
+var isAnagramFirst = function(s, t) {
     const os = s.split('').sort().join();
     const ot = t.split('').sort().join();
     
