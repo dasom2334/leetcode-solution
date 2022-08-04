148fd2bd4e4b3617d5b0a100930176f246ede8dd
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-08-05 02:39:53 +0900

    Time: 81 ms (33.33%), Space: 41.6 MB (100.00%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} p
+ * @param {number} q
+ * @return {number}
+ */
+var mirrorReflection = function(p, q) {
+	while (p%2==0 && q%2==0) {
+		p = p / 2
+		q = q / 2
+	}
+	
+	if (p%2 == 0)
+		return 2
+	
+	if (q%2 == 0)
+		return 0
+	
+	return 1
+};
