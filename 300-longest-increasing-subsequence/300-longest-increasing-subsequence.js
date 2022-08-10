cb29dd5b1b37ba5d2a6fe5008f524a6e8c4bdf80
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-08-11 00:11:34 +0900

    Time: 201 ms (67.80%), Space: 44.7 MB (15.31%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
@@ -9,13 +9,20 @@ var lengthOfLIS = function(nums) {
     // console.log(dp);
     for (let i = 0; i < nums.length; i++) {
         const index = onis[i];
-        const maxs = dp.slice(0, index).filter((e, i) => nums[i] !== nums[index]);
-        // console.log(maxs);
-        if (maxs.length == 0) {
-            dp[index] = 1;
-        } else {
-            dp[index] = Math.max(...maxs) + 1;
+        let max = 0;
+        for (let j = 0; j < index; j++) {
+            if (nums[j] !== nums[index]) {
+                max = Math.max(max, dp[j]);
+            }
         }
+        dp[index] = max + 1;
+        // const maxs = dp.slice(0, index).filter((e, i) => nums[i] !== nums[index]);
+        // // console.log(maxs);
+        // if (maxs.length == 0) {
+        //     dp[index] = 1;
+        // } else {
+        //     dp[index] = Math.max(...maxs) + 1;
+        // }
         // console.log(dp);
     }
     
