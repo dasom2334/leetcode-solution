792221a6a221d775257c48a527998276c13de6de
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-08-12 17:24:21 +0900

    Time: 103 ms (56.81%), Space: 46 MB (84.11%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.js
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.js
@@ -12,22 +12,12 @@
  */
 var isValidBST = function(root) {
     const dfs = (node, min = -Infinity, max = Infinity) => {
-        // console.log(node, min, '<', node.left, '<', node, '<', node.right, '<', max);
         if (!node.right && !node.left) {
-            // console.log(true,1,node, min, max);
             return true;   
         } else if ((node.right && (node.right.val >= max || node.right.val <= node.val)) || (node.left && (node.left.val <= min || node.left.val >= node.val))) {
-            // console.log(false, 2);
-            // console.log(false, 2,node, min, max, false,node.val, node.left.val, node.right.val, );
-            // console.log(node.right, node.right.val >= max , node.right.val <= node.val);
-            // console.log((node.left && (node.left.val <= min || node.left.val >= node.val)));
             return false;
         } else  {
-            const r = ((node.right) ? dfs(node.right, node.val, max):true ) && ((node.left)? dfs(node.left, min, node.val):true);
-            
-            // console.log(r, 3, node, min, max);
-            return r;
-            return ((node.right) ? dfs(node.right, min, node.val):true ) && ((node.left)? dfs(node.left, node.val, max):true);
+            return ((node.right) ? dfs(node.right, node.val, max):true ) && ((node.left)? dfs(node.left, min, node.val):true);
         }
     }
     
