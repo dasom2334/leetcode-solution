5532ef8d223657fddfe3c706fdeb1b4d3380dbc5
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-08-19 00:56:03 +0900

    Time: 2756 ms (8.04%), Space: 55 MB (36.73%) - LeetHub

--- 127-word-ladder/127-word-ladder.js
+++ 127-word-ladder/127-word-ladder.js
@@ -45,15 +45,16 @@ var ladderLength = function(beginWord, endWord, wordList) {
     }
     
     let levels = {};
-    let toVisit = [[beginWord, 0]];
+    let toVisit = [[beginWord, 1]];
     while(toVisit.length > 0) {
         const [word, level] = toVisit.shift();
-        if (word == endWord) return level + 1;
+        if (word == endWord) return level;
         if (word in levels) continue;
         levels[word] = level;
         for (const t of links[word]) {
-            if (!levels[t]) toVisit.push([wordList[t], level + 1]);
+            if (!levels[wordList[t]]) toVisit.push([wordList[t], level + 1]);
         }
+        console.log(word, level)
         delete links[word];
     }
     return result;
