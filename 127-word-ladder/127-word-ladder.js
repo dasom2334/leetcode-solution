9b4e2b125052c451edc52fe0b78de3780e73290c
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-08-19 00:44:18 +0900

    Time: 2466 ms (9.43%), Space: 54.7 MB (36.87%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.js
@@ -0,0 +1,61 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+     goal = wordList.indexOf(endWord);
+    if (goal == -1) return 0;
+    if (beginWord === endWord) return 1;
+    
+    let result = 0;
+    let links = {};
+    
+    for(let i = 0; i < wordList.length; i++) {
+        for(let j = i; j < wordList.length; j++) {
+            let count = 0;
+            for (let m = 0; m < beginWord.length; m++) {
+                if(wordList[i][m] == wordList[j][m]) count++;
+            }
+            
+            if (!(wordList[i] in links)) {
+                links[wordList[i]] = [];
+            }
+            if (!(wordList[j] in links)) {
+                links[wordList[j]] = [];
+            } 
+            if (count == beginWord.length - 1) {
+                links[wordList[i]].push(j);
+                links[wordList[j]].push(i);
+            }
+        }
+    }
+    if (!(beginWord in links)) {
+        links[beginWord] = [];
+        for(let j = 0; j < wordList.length; j++) {
+            let count = 0;
+            for (let m = 0; m < beginWord.length; m++) {
+                if(beginWord[m] == wordList[j][m]) count++;
+            }
+            if (count == beginWord.length - 1) {
+                links[beginWord].push(j);
+            }
+        }
+    }
+    
+    let levels = {};
+    let toVisit = [[beginWord, 0]];
+    while(toVisit.length > 0) {
+        const [word, level] = toVisit.shift();
+        if (word == endWord) return level + 1;
+        if (word in levels) continue;
+        levels[word] = level;
+        if (!(wordList[goal] in levels)) {
+            for (const t of links[word]) {
+                if (!levels[t]) toVisit.push([wordList[t], level + 1]);
+            }
+        }
+    }
+    return result;
+};
