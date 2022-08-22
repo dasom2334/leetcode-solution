77520e450f973fba0310972ae5a8ff28d1ed16cc
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-08-22 18:20:12 +0900

    Time: 138 ms (12.29%), Space: 43.8 MB (28.10%) - LeetHub

--- 342-power-of-four/342-power-of-four.js
+++ 342-power-of-four/342-power-of-four.js
@@ -4,6 +4,7 @@
  */
 var isPowerOfFour = function(n) {
     const bit = (n >>> 0).toString(2);
-    
-    return (n == 1 || (bit[0] == '1' && bit.length % 2 == 1 && parseInt(bit.slice(1)) == 0))
-};
+    // console.log((n & (n - 1)) == 0);
+    // console.log(bit.length % 2 == 1 );
+    return (n > 0 && (n & (n - 1)) == 0 && bit.length % 2 == 1 )
+}
