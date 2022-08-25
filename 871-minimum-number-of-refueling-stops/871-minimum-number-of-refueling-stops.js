5b1f8422f7e00007bcb0a4e719dd4a1220903b22
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-08-25 16:56:45 +0900

    Time: 292 ms (9.52%), Space: 49.9 MB (9.52%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.js
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.js
@@ -13,8 +13,6 @@ var minRefuelStops = function(target, startFuel, stations) {
     for (let i = 0; i < stations.length; i++) {
         const station = stations[i];
         const steps = before - station[0]
-        // for (const output in dict) {
-        // }
         for (const output of Object.keys(dict).reverse()) {
             dict[output] += steps;
             if (dict[output] < 0) {
@@ -25,17 +23,14 @@ var minRefuelStops = function(target, startFuel, stations) {
             if (!dict[next] || dict[next] < dict[output] + station[1]) {
                 dict[next] = dict[output] + station[1];
             }
-            // console.log(dict);
         }
         
         
         before = station[0];
-        // console.log(dict, steps);
     }
     
     const steps = before - target;
     
-    // console.log(dict, steps);
     for (const output in dict) {
         dict[output] += steps;
         if (dict[output] >= 0) return output; 
