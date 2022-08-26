bf3c025dea498f9b5fd97d22720a2f2714262f38
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-08-26 19:18:30 +0900

    Time: 154 ms (50.00%), Space: 50.7 MB (62.50%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.js
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.js
@@ -15,10 +15,8 @@ var isPossible = function(nums) {
             map.set(nums[i], 1);
         }
     }
-    // console.log(map);
     const ks = [...map.keys()];
     let q = new Array(map.get(ks[0])).fill(1);
-    // console.log(q)
     for (let i = 1; i < ks.length; i++) {
         if (ks[i - 1] + 1 !== ks[i]) {
             if (q.filter(e => e < 3).length > 0) return false;
@@ -28,24 +26,11 @@ var isPossible = function(nums) {
         let count = map.get(ks[i]);
         let c = map.get(ks[i]) - q.length;
         
-        // console.log(ks[i],':', map.get(ks[i]), c, q)
         if (c > 0) {
             q = [...q, ...(new Array(c).fill(0))];
-            // while (c > 0) {
-            //     q.push(0);
-            //     c--;
-            // }
         } else if (c !== 0) {
             const outs = q.splice(0, -c);
             if (outs.filter(e => e < 3).length > 0) return false;
-            // while (c < 0) {
-            //     let sft = q.shift();
-            //     if (sft < 3) {
-            //         // console.log(sft, q)
-            //         return false;
-            //     }
-            //     c++;
-            // }
         }
         q = q.map(e => e + 1);
     }
