cf930b0280ef0e11428913a97da2602f61cb2e81
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-09-05 13:33:39 +0900

    Time: 145 ms (23.77%), Space: 46.8 MB (21.96%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.js
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.js
@@ -13,13 +13,25 @@
 var levelOrder = function(root) {
     let result = [];
     
-    const dfs = (node, level) => {
-        if (node == null) return;
+//     const dfs = (node, level) => {
+//         if (node == null) return;
+//         if (result.length < level + 1) result.push([]);
+//         result[level].push(node.val);
+//         node.children.forEach(e => dfs(e, level+1));
+//     }
+    
+//     dfs(root, 0);
+    
+    
+    let tv = [[root, 0]];
+    while(tv.length > 0) {
+        const [node, level] = tv.shift();
+        
+        if (node == null) continue;
         if (result.length < level + 1) result.push([]);
         result[level].push(node.val);
-        node.children.forEach(e => dfs(e, level+1));
+        node.children.forEach(e => tv.push([e, level+1]));
     }
     
-    dfs(root, 0);
     return result;
 };
