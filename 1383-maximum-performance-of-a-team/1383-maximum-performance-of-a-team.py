60569739c33cf2061f4a6c1a33d2e36093db8fce
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-09-12 00:33:09 +0900

    Time: 1375 ms (5.07%), Space: 29.8 MB (68.78%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.py
@@ -1,15 +1,18 @@
+from queue import PriorityQueue
+
+
 class Solution:
     def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
         result = 0
         speedSum = 0
         
-        heap = []
+        heap = PriorityQueue()
         
         for eff, spd in sorted(zip(efficiency, speed), reverse=True):
             speedSum += spd
-            heapq.heappush(heap, spd)
-            if len(heap) > k:
-                speedSum -= heapq.heappop(heap)
+            heap.put(spd)
+            if heap.qsize() > k:
+                speedSum -= heap.get()
             
             result = max(result, speedSum * eff)
         
