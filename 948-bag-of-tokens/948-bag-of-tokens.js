331b2cb6a63124db3e7aef1e9988ef5bfe23e862
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-09-13 01:54:46 +0900

    Time: 151 ms (9.09%), Space: 44.3 MB (39.39%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} tokens
+ * @param {number} power
+ * @return {number}
+ */
+var bagOfTokensScore = function(tokens, power) {
+    if (tokens.length == 0) return 0;
+    if (tokens.length == 1) return (tokens[0] < power)?1:0;
+    let result = 0;
+    
+    const ts = tokens.sort((a, b) => a - b);
+    let s = 0;
+    let p = power;
+    
+    let i = 0;
+    let j = tokens.length - 1;
+    // console.log(ts);
+    while (i <= j) {
+        // console.log(p,s,i,j, ts[i],ts[j], result);
+        while (i < j && p < ts[i] && s > 0) {
+            p += ts[j];
+            s--;
+            j--;
+        }
+        if (p < ts[i]) break;
+        
+        p -= ts[i];
+        s++;        
+        i++;
+        
+        result = Math.max(s, result);
+    }
+    
+    return result;
+};
