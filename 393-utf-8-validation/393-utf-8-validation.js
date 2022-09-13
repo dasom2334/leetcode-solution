daea780392401d9b91128c9f775463751cce065e
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-09-13 17:49:58 +0900

    Time: 134 ms (27.78%), Space: 44.3 MB (58.33%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} data
+ * @return {boolean}
+ */
+var validUtf8 = function(data) {
+    let result = false;
+    
+    let nob = 0;
+    
+    for (const d of data) {
+        const bin = toBinary(d)
+        // console.log(bin);
+        if (nob == 0) {
+            for (const bit of bin) {
+                if (bit == 1) {
+                    nob++;
+                } else {
+                    break;
+                }
+            }
+            
+            if (nob == 0) continue;
+            else if (nob == 1 || nob > 4) return false;
+            else nob--;
+            
+        } else {
+            if (bin[0]+bin[1] != '10') return false;
+            nob--;
+        }
+        
+        // console.log(nob);
+        
+    }
+    
+    return nob == 0;
+};
+
+function toBinary(number) {
+    return (parseInt(number) >> 0).toString(2).padStart(8, '0');
+}
