af54221394c1880815d20f581953edace83850e3
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-09-16 21:09:31 +0900

    Time: 282 ms (88.80%), Space: 54.7 MB (99.20%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.js
@@ -8,8 +8,8 @@ var maximumScore = function(nums, multipliers) {
     
     for (let i = 0; i < multipliers.length; i++) {
         dp = new Array(dp.length + 1).fill(0).map((e, j) => {
-            let left = (j > dp.length - 1)? -Infinity:dp[j] + nums[i-j]*multipliers[i];
-            let right = (j < 1)? -Infinity:dp[j-1] + nums[nums.length - j]*multipliers[i];
+            const left = (j > dp.length - 1)? -Infinity:dp[j] + nums[i-j]*multipliers[i];
+            const right = (j < 1)? -Infinity:dp[j-1] + nums[nums.length - j]*multipliers[i];
             return Math.max(left, right);
         });
     }
