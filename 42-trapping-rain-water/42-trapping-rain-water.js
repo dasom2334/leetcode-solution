0f3f6f83540d597990e51791d7ebb049234a5b78
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-09-18 21:44:57 +0900

    Time: 114 ms (42.24%), Space: 42.8 MB (80.43%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -3,6 +3,28 @@
  * @return {number}
  */
 var trap = function(height) {
+    let i = 0
+    let j = height.length - 1;
+    let maxi = height[i];
+    let maxj = height[j];
+    let result = 0;
+    
+    while (i < j) {
+        if (maxi < maxj) {
+            i++;
+            maxi = Math.max(maxi, height[i]);
+            result += maxi - height[i]
+        } else {
+            j--;
+            maxj = Math.max(maxj, height[j]);
+            result += maxj - height[j]
+            
+        }
+    }
+    
+    return result;
+};
+var trapOld = function(height) {
     let result = 0;
     let stack = [0];
     let bars = height.slice();
