b2cefa2dda5fc2b4070543b729a8b88226963f2b
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-09-20 16:59:33 +0900

    Time: 533 ms (35.64%), Space: 90.5 MB (10.91%) - LeetHub

---
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findLength = function(nums1, nums2) {
+    let dp = new Array(nums1.length + 1).fill(0).map(e => new Array(nums2.length + 1).fill(0));
+    let result = 0;
+    for (let i = 1; i <= nums1.length; i++) {
+        console.log(nums1[i-1]);
+        for(let j = 1; j <= nums2.length; j++) {
+            if (nums1[i-1] == nums2[j-1])  {
+                dp[i][j] = dp[i-1][j-1] + 1;
+                result = Math.max(result, dp[i-1][j-1] + 1);
+            }
+        }
+    }
+    
+    return result;
+};
