c722e976c821ea6ee0c3b0b625ccb2b794f3288e
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-09-27 17:11:44 +0900

    Time: 192 ms (65.67%), Space: 58.1 MB (71.64%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.js
+++ 838-push-dominoes/838-push-dominoes.js
@@ -3,6 +3,34 @@
  * @return {string}
  */
 var pushDominoes = function(dominoes) {
+    let arr = new Array(dominoes.length).fill(0);
+    let force = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (dominoes[i] == 'R') {
+            force = arr.length;
+        } else if (dominoes[i] == 'L') {
+            force = 0;
+        } else {
+            force = Math.max(force - 1, 0);
+        }
+        arr[i] = force;
+    }
+    force = 0;
+    // console.log(arr);
+    for (let i = arr.length -1; i >= 0; i--) {
+        if (dominoes[i] == 'L') {
+            force = arr.length;
+        } else if (dominoes[i] == 'R') {
+            force = 0;
+        } else {
+            force = Math.max(force - 1, 0);
+        }
+        arr[i] -= force;
+    }
+    // console.log(arr);
+    return arr.map(e => (e > 0)?'R':((e < 0)? 'L': '.')).join('');
+};
+var pushDominoes1 = function(dominoes) {
     let arr = [...dominoes];
     let li = -1;
     let ri = li;
