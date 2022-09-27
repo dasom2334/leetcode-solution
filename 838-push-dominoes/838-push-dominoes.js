9ddf0581d800ec585c41dfa63965b5748c7b676f
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-09-27 15:11:34 +0900

    Time: 219 ms (56.72%), Space: 60.1 MB (62.69%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.js
+++ 838-push-dominoes/838-push-dominoes.js
@@ -5,49 +5,39 @@
 var pushDominoes = function(dominoes) {
     let arr = [...dominoes];
     let li = -1;
-    let ld = 'L';
     let ri = li;
-    let rd = ld;
-    // let [li, ld] ;= [-1, 'L'];
-    // let [ri, rd] = [li, ld];
-    
-
-    const toPoint = (li, ld, ri, rd, arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (dominoes[i] == '.') continue;
+        li = ri;
+        ri = i;
         let count = ri - li - 1;
-        if (ld == 'L') {
-            if (rd == 'R') return;
+        if (arr[li] != 'R') {
+            if (arr[ri] == 'R') continue;
             while (count > 0) {
                 arr[ri - count] = 'L';
                 count--;
             }
         } else {
-            if (rd == 'L') count = Math.floor(count/2);
+            if (arr[ri] == 'L') count = Math.floor(count/2);
             for (let i = count; i > 0; i--) {
-                arr[li + i] = ld;
+                arr[li + i] = arr[li];
             }
-            if (rd == 'R') return;
+            if (arr[ri] == 'R') continue;
             for (let i = count; i > 0; i--) {
-                arr[ri - i] = rd;
+                arr[ri - i] = arr[ri];
             }
         }
     }
-    for (let i = 0; i < arr.length; i++) {
-        if (dominoes[i] == '.') continue;
-        li = ri;
-        ld = rd;
-        ri = i;
-        rd = arr[i];
-        // [li, ld] = [ri, rd];
-        // [ri, rd] = [i, arr[i]];
-        toPoint(li, ld, ri, rd, arr);
-    }
     
-    li = ri;
-    ld = rd;
-    ri = arr.length;
-    rd = 'R';
+    if (arr[ri] == 'R') {
+        ri++;
+        while (ri < arr.length) {
+            arr[ri] = 'R';
+            ri++;
+        }
+    } 
     // [li, ld] = [ri, rd];
     // [ri, rd] = [arr.length, 'R'];
-    toPoint(li, ld, ri, rd, arr);
+    // toPoint(li, ld, ri, rd, arr);
     return arr.join('');
 };
