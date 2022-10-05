5cf998bfc7f47b02e42ce946cab9f6f07ffb454b
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-10-05 18:12:07 +0900

    Time: 158 ms (43.10%), Space: 48 MB (68.97%) - LeetHub

--- 623-add-one-row-to-tree/623-add-one-row-to-tree.js
+++ 623-add-one-row-to-tree/623-add-one-row-to-tree.js
@@ -13,6 +13,30 @@
  * @return {TreeNode}
  */
 var addOneRow = function(root, val, depth) {
+    if (depth == 1) {
+        const newRoot = new TreeNode(val);
+        newRoot.left = root;
+        return newRoot;
+    }
+    
+    const dfs = (node, dep) => {
+        if (node == null) return;
+        if (dep == 1) {
+            const nl = new TreeNode(val);
+            const nr = new TreeNode(val);
+            nl.left = node.left;
+            nr.right = node.right;
+            node.left = nl;
+            node.right = nr;
+            return;
+        }
+        dfs(node.left, dep - 1);
+        dfs(node.right, dep - 1);
+    }
+    dfs(root, depth - 1);
+    return root;
+};
+var addOneRow2 = function(root, val, depth) {
     if (root == null) return root;
     if (depth - 1 == 1) {
         const nl = new TreeNode(val);
