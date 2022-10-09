c57029ce5e284d5c1d283b817b34bbd84cade8c4
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-10-09 21:59:56 +0900

    Time: 147 ms (61.45%), Space: 51.1 MB (96.51%) - LeetHub

---
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {boolean}
+ */
+var findTarget = function(root, k) {
+    let toVisit = [root];
+    
+    while (toVisit.length > 0) {
+        const node = toVisit.shift();
+        if (node == null) continue;
+        
+        if (node.val !== k/2 && isExists(root, k - node.val)) {
+            return true;
+        }    
+        toVisit.push(node.left, node.right);
+    }
+    
+    return false;
+};
+    
+function isExists(root, n) {
+    let node = root;
+    while (node !== null) {
+        if (node.val == n) {
+            return true;
+        } else if (node.val > n) {
+            node = node.left;
+        } else {
+            node = node.right;
+        }
+    }
+    
+    return false;
+}
+    
