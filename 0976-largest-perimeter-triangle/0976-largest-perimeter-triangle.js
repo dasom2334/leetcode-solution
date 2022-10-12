3a075d328c66c8ab0536e093a42b231ebb74c197
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-10-12 17:37:44 +0900

    Time: 109 ms (89.73%), Space: 45.4 MB (64.53%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.js
@@ -5,13 +5,10 @@
 var largestPerimeter = function(nums) {
     nums.sort((a, b) => b - a);
     
-    for (let j = 0; j < nums.length; j++) {
-        let result = nums[j];
-        for (let i = j+2; i < nums.length; i++) {
-            if (result < nums[i] + nums[j+1]) {
-                return result + nums[i] + nums[j+1];
-            }
-        }   
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < nums[i+1] + nums[i+2]) {
+            return nums[i] + nums[i+1] + nums[i+2];
+        }
     }
     
     return 0;
