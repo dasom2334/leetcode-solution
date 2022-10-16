51452e2475f9cf791641d34d724d038df97ec124
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-10-17 05:03:56 +0900

    Time: 185 ms (62.79%), Space: 44.2 MB (65.12%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} jobDifficulty
+ * @param {number} d
+ * @return {number}
+ */
+var minDifficulty = function(jobDifficulty, d) {
+    const len = jobDifficulty.length;
+    
+    if (len == d) return jobDifficulty.reduce((a, b) => a + b);
+    if (len < d) return -1;
+    
+    const dp = new Array(len+1).fill(null).map(e => new Array(d+1).fill(-1));
+    
+    const dfs = (cur, day) => {
+        // console.log(cur, day);
+        if (dp[cur][day] != -1) return dp[cur][day];
+        if (day == 0) {
+            dp[cur][day] = (cur >= len) ? 0 : Math.max(...jobDifficulty.slice(cur));
+            return dp[cur][day];
+        }
+        let min = Infinity;
+        let max = 0;
+        for (let i = cur; i <= len - day; i++) {
+            max = Math.max(max, jobDifficulty[i]);
+            let temp = dfs(i + 1, day - 1);
+            min = Math.min(temp + max, min);
+        }
+        dp[cur][day] = min;
+        // console.log(dp);
+        return min;
+    }
+    
+    // console.log(dp);
+    
+    return dfs(0, d);
+};
