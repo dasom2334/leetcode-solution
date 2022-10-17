df49bcfbf33be80dcbb6da89cf5e661aa1951771
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-10-17 22:34:29 +0900

    Time: 66 ms (93.44%), Space: 44.5 MB (8.99%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function(sentence) {
+    const map = new Map();
+    
+    for (const s of sentence) {
+        if (!map.has(s)){
+            map.set(s, 1);
+        }
+    }
+    
+    for (const a of 'abcdefghijklmnopqrsuvwxyz') {
+        if (!map.has(a)) {
+            return false;
+        }
+    }
+    
+    return true;
+};
