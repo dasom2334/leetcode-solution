e72478bf68be225c1e0819b50c07fcac1028cd55
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-10-24 17:26:00 +0900

    Time: 2972 ms (8.39%), Space: 48.3 MB (65.04%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.js
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.js
@@ -4,16 +4,15 @@
  */
 var maxLength = function(arr) {
     const dfs = (idx = 0, str = "") => {
-        // console.log(idx, str)
-        if (idx >= arr.length) return str.length;
+        // console.log(idx, str, str.length, new Set([...str]).size);
+        if (str.length != new Set([...str]).size) return 0;
+        if (idx >= arr.length) return str.length;   
+        
+        
         let max = 0;
+        max = Math.max(dfs(idx + 1, str), max);
         for (let i = idx; i < arr.length; i++) {
-            const nstr = str + arr[i];
-            if (nstr.length != new Set([...nstr]).size) {
-                max = Math.max(dfs(i + 1, str), max);
-            } else {
-                max = Math.max(dfs(i + 1, nstr), max);
-            }
+            max = Math.max(dfs(i + 1, str + arr[i]), max);
         }
         return max;        
     }
