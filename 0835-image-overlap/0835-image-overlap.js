e19e3f8febdc38090ed24193747080f2fd9d746a
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-10-28 01:42:29 +0900

    Time: 1142 ms (8.33%), Space: 49.2 MB (87.50%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.js
+++ 0835-image-overlap/0835-image-overlap.js
@@ -4,7 +4,6 @@
  * @return {number}
  */
 var largestOverlap = function(img1, img2) {
-    let result = 0;
     
     const i1 = [];
     const i2 = [];
@@ -15,23 +14,26 @@ var largestOverlap = function(img1, img2) {
         for (let j = 0; j < img1.length; j++) {
             if (img1[i][j] == 1) i1.push([i, j]);
             if (img2[i][j] == 1) {
-                i2.push([i, j]);  
+                // i2.push([i, j]);  
                 map.set(i + "," + j, 1);
             } 
         }
     }
     if (i1.length == 0) return 0;
+    let result = 0;
     
-    for (let i = 0; i < i1.length; i++) 
-        for (let j = 0; j < i2.length; j++) {{
-            const fy = i2[j][0] - i1[i][0];
-            const fx = i2[j][1] - i1[i][1];
-            const fixKey = 'fix'+fy+','+fx;
-            if (map.has(fixKey)) continue;
-            map.set(fixKey, 1);
+    for (let i = -img1.length + 1; i < img1.length; i++) 
+        for (let j = -img1.length + 1; j < img1.length; j++) {{
+    // for (let i = 0; i < i1.length; i++) 
+    //     for (let j = 0; j < i2.length; j++) {{
+    //         const fy = i2[j][0] - i1[i][0];
+    //         const fx = i2[j][1] - i1[i][1];
+            // const fixKey = 'fix'+fy+','+fx;
+            // if (map.has(fixKey)) continue;
+            // map.set(fixKey, 1);
             let count = 0;
             for (let k = 0; k < i1.length; k++) {
-                const key = (i1[k][0] + fy) +","+ (i1[k][1] + fx);
+                const key = (i1[k][0] + i) +","+ (i1[k][1] + j);
                 if (map.has(key)) {
                     count++;
                 }
