8f311848364a7831eb48649b42b90d1567c1d239
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-10-28 19:46:36 +0900

    Time: 117 ms (97.36%), Space: 53.2 MB (64.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -6,13 +6,24 @@ var groupAnagrams = function(strs) {
     const dict = {};
     
     for (const str of strs) {
-        let s = str.split('');
-        s.sort();
-        s = s.join('');
-        // console.log(s);
-        if (!(s in dict)) dict[s] = [];
-        dict[s].push(str);
+        let count = 0;
+        for (let i = 0; i < str.length; i++) {
+            count += str.charCodeAt(i) ** 4;
+        }
+        count *= str.length;
+        if (!(count in dict)) dict[count] = [];
+        dict[count].push(str);
     }
+    
+    
+//     for (const str of strs) {
+//         let s = str.split('');
+//         s.sort();
+//         s = s.join('');
+//         // console.log(s);
+//         if (!(s in dict)) dict[s] = [];
+//         dict[s].push(str);
+//     }
     
     return Object.values(dict);
 };
