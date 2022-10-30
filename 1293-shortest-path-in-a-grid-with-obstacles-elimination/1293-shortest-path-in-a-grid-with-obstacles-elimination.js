a529322030817ca9cb572a54288b11993fb07d17
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-10-31 02:50:59 +0900

    Time: 969 ms (22.37%), Space: 44.6 MB (100.00%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.js
@@ -0,0 +1,45 @@
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number}
+ */
+var shortestPath = function(grid, k) {
+    const dp = new Array(grid.length).fill(null).map(e => new Array(grid[0].length).fill(null).map(e2 => new Array(2).fill(Infinity)));
+    dp[grid.length -1][grid[0].length-1][0] = -1;
+    const dfs = (m, n, step, cnt) => {
+        if (m == grid.length - 1 && n == grid[0].length - 1) {
+            dp[m][n][0] = step;
+            dp[m][n][1] = cnt;
+            return;
+        }
+        if (m >= grid.length || n >= grid[0].length || m < 0 || n < 0) return;
+        
+        if (grid[m][n] == 1) {
+            cnt--;
+            if (cnt < 0) return;
+        }
+        if (dp[m][n][0] == step && dp[m][n][1] >= cnt || dp[m][n][0] < step) {
+            return;
+        } else {
+            dp[m][n][0] = step;
+            dp[m][n][1] = cnt;
+        }
+        
+        dfs(m - 1, n, step+1, cnt);
+        dfs(m, n - 1, step+1, cnt);
+        dfs(m + 1, n, step+1, cnt);
+        dfs(m, n+1, step+1, cnt);
+//         const wasd = [dfs(m - 1, n, step+1, cnt), dfs(m, n - 1, step+1, cnt), dfs(m + 1, n, step+1, cnt), dfs(m, n+1, step+1, cnt)].filter(e => e !== null);
+        
+//         if (wasd.length == 0) {
+//             return null;
+//         } else {
+//             return Math.min(...wasd);
+//         }
+    }
+    dfs(0, 0, 0, k);
+    // console.log(dp);
+    // console.log(dp[grid.length -1][grid[0].length-1]);
+    // return dfs(0, 0, 0, k) ?? -1;
+    return dp[grid.length -1][grid[0].length-1][0];
+};
