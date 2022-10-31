73e5b3f80dea843ed419cc613031e5c6bd0a5b79
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-10-31 12:51:59 +0900

    Time: 116 ms (43.23%), Space: 44.4 MB (33.46%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.js
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.js
@@ -4,25 +4,9 @@
  */
 var isToeplitzMatrix = function(matrix) {
     
-    for (let i = 0; i < matrix[0].length; i++) {
-        const n = matrix[0][i];
-        let j = 1;
-        while (j < matrix.length && i+j < matrix[0].length) {
-            
-            // console.log(n, matrix[j][i+j], j, i+j);
-            if (n !== matrix[j][i+j]) return false;
-            j++;
-        }
-    }
-    
-    for (let i = 1; i < matrix.length; i++) {
-        const n = matrix[i][0];
-        let j = 1;
-        while (i+j < matrix.length && j < matrix[0].length) {
-            // console.log(n, matrix[i+j][j], i+j, j, '?');
-            if (n !== matrix[i+j][j]) return false;
-            j++;
-        }
+    for (let i = 0; i < matrix.length - 1; i++) {
+        const matched = matrix[i].slice(0, matrix[0].length - 1).filter((e, j) => matrix[i+1][j+1] == e);
+        if (matched.length < matrix[0].length -1) return false;
     }
     
     return true;
