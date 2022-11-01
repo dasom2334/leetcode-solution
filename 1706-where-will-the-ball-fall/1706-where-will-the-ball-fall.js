3b8cf9d2d08a666e1b360171832711e734e0d61a
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-01 19:40:38 +0900

    Time: 117 ms (48.56%), Space: 44.8 MB (32.37%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
@@ -5,20 +5,52 @@
 
 var findBall = function(grid) {
     const result = new Array(grid[0].length).fill(-1);
+
+    for (let b = 0; b < grid[0].length; b++) {
+        let curC = b;
+        let r = 0;
+        for (; r < grid.length; r++) {
+            let nextC = curC + grid[r][curC];
+            if (grid[r][curC] !== grid[r][nextC]) {
+                break;
+            }
+            
+            curC = nextC;
+        }
+        
+        
+        if (r == grid.length) result[b] = curC;
+    }
+    
+    return result;
+}
+var findBall2 = function(grid) {
+    // const dp = new Map();
+    const result = new Array(grid[0].length).fill(-1);
     
     for (let b= 0; b < grid[0].length; b++) {
         let c = b;
         let r = 0;
         for (; r < grid.length; r++) {
-            let path = c + grid[r][c];
+            const key = [r, c].join();
+            if (dp.has(key)) {
+                break;
+            }
+            
+            dp.set(key, 1);
+            let path = c;
+            if (grid[r][c] == 1) path++;
+            else path--;
             
             if (grid[r][path] !== grid[r][c]) {
+                dp.set([r, path].join(), 1);
                 break;
             }
             
             c = path;
             
         }
+        // console.log(grid.length, r);
         if (r == grid.length) result[b] = c;
     }
     
