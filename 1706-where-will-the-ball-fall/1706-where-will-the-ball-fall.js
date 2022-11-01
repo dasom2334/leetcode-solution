e24dedefc9f1a80d47e923eb5daf32db8d65ff59
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-01 19:39:37 +0900

    Time: 133 ms (19.39%), Space: 43.8 MB (96.47%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
@@ -2,26 +2,25 @@
  * @param {number[][]} grid
  * @return {number[]}
  */
+
 var findBall = function(grid) {
-    const dp = new Map();
+    // const dp = new Map();
     const result = new Array(grid[0].length).fill(-1);
     
     for (let b= 0; b < grid[0].length; b++) {
         let c = b;
         let r = 0;
         for (; r < grid.length; r++) {
-            const key = [r, c].join();
-            if (dp.has(key)) {
-                break;
-            }
+            // const key = [r, c].join();
+            // if (dp.has(key)) {
+            //     break;
+            // }
             
-            dp.set(key, 1);
-            let path = c;
-            if (grid[r][c] == 1) path++;
-            else path--;
+            // dp.set(key, 1);
+            let path = c + grid[r][c];
             
             if (grid[r][path] !== grid[r][c]) {
-                dp.set([r, path].join(), 1);
+                // dp.set([r, path].join(), 1);
                 break;
             }
             
