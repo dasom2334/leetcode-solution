085b393754d6901b65f00830bce1f813adf04d9d
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-01 19:42:13 +0900

    Time: 112 ms (57.37%), Space: 44.1 MB (91.35%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
@@ -3,27 +3,27 @@
  * @return {number[]}
  */
 
-// var findBall = function(grid) {
-//     const result = new Array(grid[0].length).fill(-1);
-//     for (let b = 0; b < grid[0].length; b++) {
-//         let curC = b;
-//         let r = 0;
-//         for (; r < grid.length; r++) {
-//             let nextC = curC + grid[r][curC];
-//             if (grid[r][curC] !== grid[r][nextC]) {
-//                 break;
-//             }
-            
-//             curC = nextC;
-//         }
-        
-        
-//         if (r == grid.length) result[b] = curC;
-//     }
-    
-//     return result;
-// }
 var findBall = function(grid) {
+    const result = new Array(grid[0].length).fill(-1);
+    for (let b = 0; b < grid[0].length; b++) {
+        let curC = b;
+        let r = 0;
+        for (; r < grid.length; r++) {
+            let nextC = curC + grid[r][curC];
+            if (grid[r][curC] !== grid[r][nextC]) {
+                break;
+            }
+            
+            curC = nextC;
+        }
+        
+        
+        if (r == grid.length) result[b] = curC;
+    }
+    
+    return result;
+}
+var findBall2 = function(grid) {
     const result = new Array(grid[0].length).fill(-1);
     
     for (let b= 0; b < grid[0].length; b++) {
