fc46f65b1070fb367f899371c4e4dc2e2d518b15
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-02 23:06:27 +0900

    Time: 100 ms (35.59%), Space: 42.2 MB (38.98%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} start
+ * @param {string} end
+ * @param {string[]} bank
+ * @return {number}
+ */
+var minMutation = function(start, end, bank) {
+    let toVisit = [[start, 0]];
+    let result = Infinity;
+    const map = new Map();
+    while (toVisit.length > 0) {
+        const [str, cnt] = toVisit.shift();
+        if (map.has(str)) {
+            continue;
+        } else {
+            map.set(str, 1);
+        }
+        if (str == end) {
+            result = Math.min(result, cnt);
+            continue;
+        }
+        
+        const bs = bank.filter(e => unMatchedCnt(e, str) == 1);
+        
+        // console.log(bs);
+        bs.forEach(e => toVisit.push([e, cnt + 1]));
+        // console.log(toVisit);
+        
+    }
+    
+    return (result === Infinity)? -1:result;
+};
+    
+function unMatchedCnt (str1, str2) {
+    let cnt = 0;
+    str1.split('').forEach((e, i) => {
+        if (e == str2[i]) cnt++;
+    })
+    return str1.length - cnt;
+}
