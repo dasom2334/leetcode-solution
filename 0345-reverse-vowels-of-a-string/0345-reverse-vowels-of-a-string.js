6390d6e47ad074540ebaca685c5395cba9e19b25
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-04 21:13:31 +0900

    Time: 134 ms (63.22%), Space: 48.5 MB (60.23%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+
+    const vowels = /[a|e|i|o|u]/i;
+    let r = s.length - 1;
+    const result = s.split('');
+    for (let l = 0; l < r; l++) {
+        if (vowels.test(s[l])) {
+            while (!vowels.test(s[r])) {
+                r--;
+            }
+            let temp = s[l];
+            result[l] = s[r];
+            result[r] = temp;
+            r--;
+        }
+    }    
+    return result.join('');
+};
