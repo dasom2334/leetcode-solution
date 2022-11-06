df87971cf0f580e8671e7f235591780377c95ac3
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-07 00:18:28 +0900

    Time: 8156 ms (7.44%), Space: 54.3 MB (11.27%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.js
+++ 0212-word-search-ii/0212-word-search-ii.js
@@ -12,7 +12,6 @@ var findWords = function(board, words) {
     const trie = {};
     const dp = new Array(m).fill(null).map(e => new Array(n).fill(0));
     const result = new Set();
-    const map = new Map();
     
     
     // for (const word of words) {
@@ -22,7 +21,6 @@ var findWords = function(board, words) {
     // }
     
     for (const word of words) {
-        
         let tr = trie;
         for (let i = word.length - 1; i >= 0; i--) {
             if (!(word[i] in tr)) tr[word[i]] = {};
@@ -33,53 +31,31 @@ var findWords = function(board, words) {
     
     
     // console.log(map);
-    const doTrie = (str) => {
-        if (map.has(str)) {
-            return;
-        } else {
-            map.set(str, 1);
-        }
-        for (let i = str.length - 1; i >= 0; i--) {
-            if (!(str[i] in trie)) continue;
-            
-            let tr = trie;
-            let j = i;
-            while (str[j] in tr) {
-                tr = tr[str[j]];
-                if (tr['word'] === true) {
-                    result.add(str.slice(j, i+1));  
-                } 
-                j--;
-            }
-        }
-    }
-    
     const dfs = (y, x, str = "") => {
-        if (y >= m || x >= n || y < 0 || x < 0 || dp[y][x] === 1 || str.length >= maxL) {
-            return false;
-        }
+        if (y >= m || x >= n || y < 0 || x < 0 || dp[y][x] === 1 || str.length >= maxL) return;
         dp[y][x] = 1;
         
         str += board[y][x];
         
+        let tr = trie;
+        let xi = str.length - 1;
+        let i = str.length - 1;
+        for (; i >= 0; i--) {
+            if (!(str[i] in tr)) break;
+            tr = tr[str[i]];
+        }
+        if (i === -1 && tr['word'] === true) result.add(str);
         // if (map.get(board[y][x])?.includes(str)) {
         //     result.add(str);
         // }
         
-        const wasd = [dfs(y+1, x, str), dfs(y-1, x, str), dfs(y, x+1, str), dfs(y, x-1, str)];
-        // console.log(wasd);
-        if (!wasd[0] && !wasd[1] && !wasd[2] && !wasd[3]) {
-        // if (!dfs(y+1, x, str) && !dfs(y-1, x, str) && !dfs(y, x+1, str) && !dfs(y, x-1, str)) {
-        // if (!wasd.includes(true)) {
-            // console.log(str);
-            doTrie(str); 
-        }
+        dfs(y+1, x, str);
+        dfs(y-1, x, str);
+        dfs(y, x+1, str);
+        dfs(y, x-1, str);
         
         dp[y][x] = 0;
-        return true;
     };
-    
-    
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
             dfs(i, j);
