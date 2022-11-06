d87e4897d34b90b4ea924c71803452a386286242
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-06 15:48:12 +0900

    Time: 93 ms (88.89%), Space: 48.5 MB (11.11%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.js
+++ 0899-orderly-queue/0899-orderly-queue.js
@@ -7,17 +7,20 @@ var orderlyQueue = function(s, k) {
     if (k > 1) {
         return s.split('').sort().join('');
     }
+    
+    
     const q = s.split('');
-    let result = s;
-
+    // let result = s;
+    const arr = [s];
     
     for (let i = 0; i < s.length - 1; i++) {
         q.push(q.shift());
-        const temp = q.join('');
-        if (result.localeCompare(temp) == 1) {
-            result = temp;
-        }
+        arr.push(q.join(''));
+        // const temp = q.join('');
+        // if (result.localeCompare(temp) == 1) {
+        //     result = temp;
+        // }
     }
-    return result;
+    return arr.sort()[0];
 };
 
