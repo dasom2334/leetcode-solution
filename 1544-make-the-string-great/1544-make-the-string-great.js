d4c2499ea03e923d0f73d37b57173a035037f7e2
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-09 01:39:46 +0900

    Time: 135 ms (18.44%), Space: 45.3 MB (26.95%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.js
+++ 1544-make-the-string-great/1544-make-the-string-great.js
@@ -12,8 +12,6 @@ var makeGood = function(s) {
         for (let i = 0; i < str.length - 1; i++) {
             const regexp = new RegExp(str[i], 'i');
             if (str[i] !== str[i+1] && regexp.test(str[i+1])) {
-                // console.log(regexp.test(str[i+1]), str[i], str[i+1]);
-                // console.log(str.slice(0, i), str.slice(i + 2))
                 str = str.slice(0, i) + str.slice(i + 2);
                 changed = true;
                 break;
