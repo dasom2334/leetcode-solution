4b85923363180fc46bf1af97cc3c4bfcb089b4bb
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-11 14:06:17 +0900

    Time: 120 ms (64.95%), Space: 44.2 MB (94.42%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,6 +3,19 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+    let result = 0;
+    
+    
+    for (let i = 1; i <= nums.length; i++) {
+        if (nums[i-1] !== nums[i]) {
+            nums[result] = nums[i-1];
+            result++;
+        }
+    }
+    
+    return result;
+};
+var removeDuplicates2 = function(nums) {
     let before = -101;
     let result = 0;
     
