a600c6ecfb1113867e12cc461367348c3a89076b
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-21 23:48:52 +0900

    Time: 217 ms (49.61%), Space: 50.6 MB (76.74%) - LeetHub

---
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.js
@@ -0,0 +1,22 @@
+/**
+ * @param {character[][]} maze
+ * @param {number[]} entrance
+ * @return {number}
+ */
+var nearestExit = function(maze, entrance) {
+    const toVisit = [[...entrance, 0]];
+    // let 
+    while (toVisit.length > 0) {
+        const [x, y, count] = toVisit.shift();
+        if (x === maze.length || x === -1 || y === maze[0].length || y === -1) {
+            if (count > 1) return count - 1;    
+            else continue;
+        }
+        if (maze[x][y] == "+") continue;
+        // console.log(x, y, count);
+        maze[x][y] = "+";
+        toVisit.push([x + 1, y, count + 1], [x - 1, y, count + 1], [x, y + 1, count + 1], [x, y - 1, count + 1]);
+    }
+    
+    return -1;
+};
