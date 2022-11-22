3ed8e7c9422d823a7dee029a3cda77290a1030d2
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-23 00:37:37 +0900

    Time: 983 ms (17.58%), Space: 85.1 MB (5.08%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function(n) {
+    const minSqrt = Math.floor(Math.sqrt(n));
+    const squares = new Array(minSqrt).fill(0).map((e, i) => (i + 1) ** 2);
+    const toVisit = [[0, 0]];
+    const visited = new Map();
+    
+    while (toVisit.length > 0) {
+        const [sum, cnt] = toVisit.shift();
+        if (sum > n) continue;
+        if (sum === n) return cnt;
+        toVisit.push(...squares.filter(e => {
+            if (visited.has(sum + e)) {
+                return false;
+            } else {
+                visited.set(sum+e, 1);
+                return true;
+            }
+        }).map(e => [sum+e, cnt+1]));
+    }
+    
+    return n;
+};
