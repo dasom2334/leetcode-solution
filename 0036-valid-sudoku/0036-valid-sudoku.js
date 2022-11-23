9d7e4bc8bf6de209bf96a205cb07327924b40123
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-24 02:30:33 +0900

    Time: 109 ms (77.03%), Space: 45.1 MB (62.04%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -22,11 +22,7 @@ var isValidSudoku = function(board) {
         for (let j = 0; j < nine; j++) {
             if (j % 3 === 0) boxNum++;
             const num = board[i][j];
-            // console.log(i, j, boxNum, num)
             if (num === '.') continue;
-            // if (num in rowDict[i]) console.log('row', num, rowDict[i]);
-            // if (num in colDict[j]) console.log('col', num, colDict[j]);
-            // if (num in boxDict[boxNum]) console.log('box', num, boxDict[boxNum]);
             if (num in rowDict[i]) return false;
             if (num in colDict[j]) return false;
             if (num in boxDict[boxNum]) return false;
