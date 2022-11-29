14cffc324c7c99d8f60cecb0f8ba5a59645f7740
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-29 22:24:58 +0900

    Time: 886 ms (15.66%), Space: 118.5 MB (11.11%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.js
@@ -0,0 +1,43 @@
+
+var RandomizedSet = function() {
+    this.set = new Set();
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    if (this.set.has(val)) return false;
+    this.set.add(val);
+    return true;
+};
+// function getRandomInt(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+// }
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    if (!this.set.has(val)) return false;
+    this.set.delete(val);
+    return true;
+};
+
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    return [...this.set][Math.floor(Math.random() * (this.set.size))];
+};
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
