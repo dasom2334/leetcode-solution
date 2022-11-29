3cda7aca064dc9ce0f56886af3c3cd523e3592fa
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-29 22:33:01 +0900

    Time: 846 ms (20.93%), Space: 119 MB (9.82%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.js
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.js
@@ -12,11 +12,6 @@ RandomizedSet.prototype.insert = function(val) {
     this.set.add(val);
     return true;
 };
-// function getRandomInt(min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
-// }
 /** 
  * @param {number} val
  * @return {boolean}
