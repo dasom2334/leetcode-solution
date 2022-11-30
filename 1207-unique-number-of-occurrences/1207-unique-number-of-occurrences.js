95d335709a705de7106d113b44a105c1c60c6c52
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-11-30 23:37:06 +0900

    Time: 107 ms (26.56%), Space: 42.1 MB (92.80%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -7,6 +7,19 @@ var uniqueOccurrences = function(arr) {
     for (const num of arr) {
         map.set(num, (map.has(num)?map.get(num):0) + 1)
     }
+    const map2 = new Map();
+    for (const [_, cnt] of map) {
+        if(map2.has(cnt)) return false;
+        map2.set(cnt, 1);
+    }
+    
+    return true;
+};
+var uniqueOccurrencesOld = function(arr) {
+    const map = new Map();
+    for (const num of arr) {
+        map.set(num, (map.has(num)?map.get(num):0) + 1)
+    }
     const set = new Set();
     for (const [_, cnt] of map) {
         set.add(cnt);
