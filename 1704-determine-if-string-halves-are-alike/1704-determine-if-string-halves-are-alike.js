779d35419f5fdef9429f6c3ea679d21590261485
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-12-02 00:20:27 +0900

    Time: 116 ms (30.08%), Space: 44.4 MB (19.14%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -4,9 +4,9 @@
  */
 var halvesAreAlike = function(s) {
     const vowels = [...'aeiou'];
-    const str = s.toLowerCase();
-    const a = str.slice(0, s.length / 2).split('').filter(e => vowels.includes(e));
-    const b = str.slice(s.length / 2).split('').filter(e => vowels.includes(e));
+    const str = [...s.toLowerCase()];
+    const a = str.slice(0, s.length / 2).filter(e => vowels.includes(e));
+    const b = str.slice(s.length / 2).filter(e => vowels.includes(e));
     
     return a.length === b.length;
 };
