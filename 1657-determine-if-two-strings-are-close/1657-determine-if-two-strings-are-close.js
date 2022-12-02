79517c77098f264de69e0d8a3bdb61795ef2fc2e
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-12-03 00:42:59 +0900

    Time: 392 ms (42.42%), Space: 53.8 MB (39.39%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
@@ -29,6 +29,7 @@ var closeStrings = function(word1, word2) {
     const word2Counts = Object.values(word2Dict).sort();
     const word1Keys = Object.keys(word1Dict).sort();
     const word2Keys = Object.keys(word2Dict).sort();
+    if (word1Keys.length !== word2Keys.length) return false;
     // console.log(word1Counts, word2Counts)
     for (let i = 0; i < word1.length; i++) {
         if (word1Counts[i] !== word2Counts[i]) return false;
