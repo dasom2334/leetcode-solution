1f319c55a7d6cd43c0b2a382086d9f6504b7ac92
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-12-03 01:00:28 +0900

    Time: 237 ms (72.73%), Space: 54.2 MB (39.39%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
@@ -6,6 +6,38 @@
 var closeStrings = function(word1, word2) {
     if (word1.length !== word2.length) return false;
     
+    const word1Map = new Map();
+    const word2Map = new Map();
+    
+    [...word1].forEach(e => {
+        word1Map.set(e, (word1Map.has(e)?word1Map.get(e):0) + 1);
+    });
+    
+    [...word2].forEach(e => {
+        word2Map.set(e, (word2Map.has(e)?word2Map.get(e):0) + 1);
+    });
+    for (const c of [...'qwertyuiopasdfghjklzxcvbnm']) {
+        if (word1Map.has(c) !== word2Map.has(c)) return false;
+    }
+    const word1arr = [...word1Map];
+    const word2arr = [...word2Map];
+    
+    if (word1arr.length !== word2arr.length) return false;
+    const word1Counts = word1arr.map(e => e[1]).sort();
+    const word2Counts = word2arr.map(e => e[1]).sort();
+    const word1Keys = word1arr.map(e => e[0]).sort();
+    const word2Keys = word2arr.map(e => e[0]).sort();
+    for (let i = 0; i < word1arr.length; i++) {
+        if (word1Counts[i] !== word2Counts[i]) return false;
+        if (word1Keys[i] !== word2Keys[i]) return false;
+    }
+    
+    
+    return true;
+};
+var closeStringsObject = function(word1, word2) {
+    if (word1.length !== word2.length) return false;
+    
     const word1Dict = {};
     const word2Dict = {};
     
