cc6df104cbaf17dad2ed02674d9259781b653b8b
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-12-11 01:35:07 +0900

    Time: 186 ms (76.67%), Space: 69.9 MB (90.00%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxProduct = function(head) {
+    const totalSum = (function dfsSum(node) {
+        if (!node) return 0;
+        return dfsSum(node.left) + dfsSum(node.right) + node.val;
+    })(head);
+    
+    let result = 0;
+    const modulo = 1000000007;
+    
+    const dfs = (node) => {
+        if (!node) return 0;
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+        result = Math.max(result, (totalSum - left) * left, (totalSum - right) * right);
+        // result %= modulo;
+        return left + right + node.val;
+    }
+    dfs(head);
+    return result % modulo;
+};
