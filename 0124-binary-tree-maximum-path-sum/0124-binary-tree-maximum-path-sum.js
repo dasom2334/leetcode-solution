aeac5ad7e0935e836cdee1862f683aec02d00589
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-12-12 01:34:13 +0900

    Time: 142 ms (32.99%), Space: 51.5 MB (70.22%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+    if (!root.left && !root.right) return root.val;
+    let result = root.val;
+    
+    const dfs = (node) => {
+        if (!node) return 0;
+        
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+        
+        result = Math.max(result, node.val, left + right + node.val, left + node.val, right + node.val);
+        
+        return Math.max(node.val, left + node.val, right + node.val);
+        
+    }
+    
+    dfs(root);
+    return result;
+};
