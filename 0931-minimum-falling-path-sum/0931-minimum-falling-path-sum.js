288c785ddd2fa3eea3354ffe1daf61925c7bda52
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-12-13 23:35:49 +0900

    Time: 1036 ms (5.00%), Space: 44.1 MB (67.00%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
@@ -3,7 +3,21 @@
  * @return {number}
  */
 var minFallingPathSum = function(matrix) {
-    const dp = new Array(matrix.length).fill(0).map(e => new Array(matrix[0].length).fill(Infinity));
+    
+    for (let i = 1; i < matrix.length; i++) {
+        for (let j = 0; j < matrix.length; j++) {
+            matrix[i][j] = Math.min(
+                matrix[i+1][j-1] || Infinity,
+                matrix[i+1][j],
+                matrix[i+1][j+1] || Infinity
+            )
+        }
+    }
+    return Math.min(...matrix.at(-1))
+    
+};
+var minFallingPathSum = function(matrix) {
+    const dp = new Array(matrix.length).fill(0).map(e => new Array(matrix.length).fill(Infinity));
     
     const dfs = (i, j, s) => {
         if (i >= matrix.length || j < 0 || j >= matrix[0].length) return;
@@ -17,7 +31,7 @@ var minFallingPathSum = function(matrix) {
         
         dfs(i+1, j-1, sum);
         dfs(i+1, j, sum);
-        dfs(i+1, j+1, sum)
+        dfs(i+1, j+1, sum);
         
     }
     
