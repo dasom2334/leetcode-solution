47ecbfcb6492201fc4b18d0bf87e0b710e3cdaeb
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-12-13 23:06:40 +0900

    Time: 98 ms (32.10%), Space: 42.3 MB (12.56%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,6 +3,17 @@
  * @return {number}
  */
 var climbStairs = function(n) {
+    const steps = new Map();
+    steps.set(1, 1);
+    steps.set(2, 2);
+    
+    for (let i = 3; i <= n; i++) {
+        steps.set(i, steps.get(i - 2) + steps.get(i-1));
+    }
+    
+    return steps.get(n);
+};
+var climbStairsObject = function(n) {
     const steps = {
         1: 1,
         2: 2
