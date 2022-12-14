ad62604e428951e05aebb58e6714ca3a30b3e729
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2022-12-15 01:09:12 +0900

    Time: 65 ms (89.38%), Space: 41.7 MB (85.60%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    if (nums.length > 2) nums[2] += nums[0];
+    for (let i = 3; i < nums.length; i++) {
+        nums[i] += Math.max(nums[i-2], nums[i-3])
+    }
+    // console.log(nums)
+    return Math.max(...nums)
+};
