c97406de656adac818ec71a67fd896da45e9452e
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2023-01-04 03:21:21 +0900

    Time: 90 ms (71.03%), Space: 46.5 MB (42.99%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
@@ -2,12 +2,12 @@
  * @param {string[]} strs
  * @return {number}
  */
-var minDeletionSize = function(strs) { 
+var minDeletionSizeOld = function(strs) { 
     const splits = strs.map(e => e.split(''));
     const arranged = new Array(strs[0].length).fill(0).map((elem, idx) => splits.map((e) => e[idx]));
     return arranged.filter(e => e.join('') !== e.sort().join('')).length;
 };
-var minDeletionSizeOld = function(strs) {
+var minDeletionSize = function(strs) {
     let result = 0;
     for(let i = 0; i < strs[0].length; i++){
          for(let j = 0; j < strs.length - 1; j++){
