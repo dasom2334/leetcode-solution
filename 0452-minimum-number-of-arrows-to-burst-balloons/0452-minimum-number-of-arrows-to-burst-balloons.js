38eece42025e17ca70f302f130854cd0bb6db59b
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2023-01-06 06:34:17 +0900

    Time: 518 ms (10.07%), Space: 74.5 MB (45.64%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function(points) {
+    let result = 0;
+    let max = -Infinity;
+    
+    points.sort((a, b) => a[0] - b[0])
+    
+    for (const [s, e] of points) {
+        if (max < s) {
+            result += 1;
+            max = Infinity;
+        }        
+        max = Math.min(max, e);
+    }
+    return result;
+};
