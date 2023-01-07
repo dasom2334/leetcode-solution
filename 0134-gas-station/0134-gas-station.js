b504a534d15d52b382770deafabd3d9d984af2e9
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2023-01-08 00:02:36 +0900

    Time: 288 ms (11.43%), Space: 61 MB (5.18%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[]} gas
+ * @param {number[]} cost
+ * @return {number}
+ */
+var canCompleteCircuit = function(gas, cost) {
+    const start = gas.map((e, i) => e - cost[i])
+    const used = gas.map((e, i) => cost.at(i - 1));
+    
+    // console.log(used, start, test)
+    const test = new Array(gas.length).fill(0).map((e, i) => i).filter(e => gas[e] - cost[e] >= 0).sort((a, b) => cost.at(b - 1) - cost.at(a - 1))
+    console.log(test)
+    // for (let i = 0; i < gas.length; i++) {
+    //     if (start[i] > 0 && used) return i;
+    // }
+    
+    for (let idx = 0; idx < test.length; idx++) {
+        let isComplete = true
+        const i = test[idx];
+        let cur = gas[i];
+        for (let j = i; j < gas.length + i; j++) {
+            const ci = (gas.length <= j)?j - gas.length:j;
+            const ci2 = (gas.length <= j + 1)?j + 1 - gas.length:j + 1;
+            cur -= cost[ci]
+            if (cur < 0) {
+                isComplete = false;
+                break;
+            }
+            cur += gas[ci2]
+        }
+        if (isComplete) return i;
+    }
+    return -1;
+};
+var canCompleteCircuitTimeLimit = function(gas, cost) {
+    // const used = gas.map((e, i) => e - cost[i]);
+    // console.log(used)
+    for (let i = 0; i < gas.length; i++) {
+        let isComplete = true
+        let cur = gas[i];
+        // console.log(cur, '---')
+        for (let j = i; j < gas.length + i; j++) {
+            const ci = (gas.length <= j)?j - gas.length:j;
+            const ci2 = (gas.length <= j + 1)?j + 1 - gas.length:j + 1;
+            cur -= cost[ci]
+            if (cur < 0) {
+                // console.log(cur)
+                isComplete = false;
+                break;
+            }
+            cur += gas[ci2]
+            // console.log('-', cost[ci], '+', gas[ci2], cur, ci)
+        }
+        if (isComplete) return i;
+    }
+    
+    return -1;
+};
