007ba7c634e3f979def8b116c2adc49a3ce0384b
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2023-01-08 22:37:48 +0900

    Time: 85 ms (96.52%), Space: 48.1 MB (71.30%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxPoints = function(points) {
+    if (points.length === 1) return 1;
+    let result = 0;
+    
+    for (let i = 0; i < points.length; i++) {
+        const map = new Map();
+        for (let j = 0; j < points.length; j++) {
+            const inc = (points[j][1] - points[i][1]) / (points[j][0] - points[i][0]);
+            map.set(inc, (map.get(inc) || 0) + 1);
+        }
+        // console.log(map, Array.from(map.values()))
+        result = Math.max(result - 1, ...Array.from(map.values())) + 1
+    }
+    // console.log(map.values())
+    return result
+};
