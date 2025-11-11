e03974a1cef4c588a00b278412fac86a60494d12
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2025-11-11 17:21:25 +0900

    Time: 2 ms (53.1%), Space: 55.7 MB (52.07%) - LeetHub

---
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.ts
@@ -0,0 +1,14 @@
+function countOperations(num1: number, num2: number): number {
+    let cnt = 0;
+    let n1 = num1;
+    let n2 = num2;
+    while(n1 > 0 && n2 > 0) {
+        if (n1 > n2) {
+            n1 -= n2;
+        } else {
+            n2 -= n1;
+        }
+        cnt++;
+    }
+    return cnt;
+};
