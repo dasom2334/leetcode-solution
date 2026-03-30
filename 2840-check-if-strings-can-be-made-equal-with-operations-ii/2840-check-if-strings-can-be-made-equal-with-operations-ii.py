2c1d9348a761f50f014e9f37842f1122a82d4d71
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2026-03-30 23:57:19 +0900

    Time: 61 ms (79.1%), Space: 20.2 MB (50%) - LeetHub

--- 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.py
@@ -1,16 +1,6 @@
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
-        q11, q12 = [], []
-        q21, q22 = [], []
+        q11, q12 = Counter(s1[::2]), Counter(s1[1::2]) 
+        q21, q22 = Counter(s2[::2]), Counter(s2[1::2]) 
 
-        for i in range(len(s1)):
-            if i % 2 == 0:
-                q11.append(s1[i])
-            else:
-                q12.append(s1[i])
-        for i in range(len(s2)):
-            if i % 2 == 0:
-                q21.append(s2[i])
-            else:
-                q22.append(s2[i])
-        return "".join(sorted(q11)) == "".join(sorted(q21)) and "".join(sorted(q12)) == "".join(sorted(q22))
+        return q11 == q21 and q12 == q22
