25c5e6e22dbbe37300ed38f7987721a9ff1ba40b
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2026-04-07 19:10:09 +0900

    Time: 44 ms (27.93%), Space: 24 MB (20.72%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.py
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.py
@@ -1,7 +1,13 @@
+from collections import defaultdict
+from bisect import bisect_left, bisect_right
+
 class Solution:
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
-        obs = set()
-        obs.update(map(tuple, obstacles))
+        xs_by_y = defaultdict(list)
+        ys_by_x = defaultdict(list)
+        for x, y in sorted(obstacles):
+            xs_by_y[x].append(y)
+            ys_by_x[y].append(x)
 
         turns = [
             [0, 1],
@@ -19,16 +25,42 @@ class Solution:
             elif command == -2:
                 turn_pointer = (turn_pointer - 1) % 4
             else:
-                for i in range(command):
-                    x, y = position
+                x, y = position
+                need_left = turn_pointer == 2 or turn_pointer == 3
+                need_x = turn_pointer == 0 or turn_pointer == 2
+                bs = bisect_left if need_left else bisect_right
+                obstacles_by_move = xs_by_y[x] if need_x else ys_by_x[y]
+                base_point = position[1] if need_x else position[0]
+                obstacle = bs(obstacles_by_move, base_point)
+                # print(
+                #     x, y,
+                #     position,
+                #     obstacle, 
+                #     base_point, 
+                #     obstacles_by_move,
+                #     obstacles_by_move[obstacle] if obstacle < len(obstacles_by_move) else 'hihi'
+                # )
+                x += turns[turn_pointer][0] * command
+                y += turns[turn_pointer][1] * command
+                if need_left:
+                    obstacle -= 1
+                if 0 <= obstacle < len(obstacles_by_move):
+                    obs = obstacles_by_move[obstacle]
+                    if need_x:
+                        target = y
+                        if need_left:
+                            y = obs + 1 if obs >= target else target
+                        else:
+                            y = obs - 1 if obs <= target else target
 
-                    x += turns[turn_pointer][0]
-                    y += turns[turn_pointer][1]
-                    # print(position, (x, y), obs) 
-                    if (x, y) in obs:
-                        break
-                    position[0] = x
-                    position[1] = y
+                    else:
+                        target = x
+                        if need_left:
+                            x = obs + 1 if obs >= target else target
+                        else:
+                            x = obs - 1 if obs <= target else target
+                position[0] = x
+                position[1] = y
                 a, b = position
                 result = max(result, a ** 2 + b ** 2)
         return result
