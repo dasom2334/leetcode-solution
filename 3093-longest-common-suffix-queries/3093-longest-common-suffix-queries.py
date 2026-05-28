d9bdc4fefd139249f162d42d67a59b53594c47e5
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2026-05-28 17:21:23 +0900

    Time: 1643 ms (35.85%), Space: 130.4 MB (92.45%) - LeetHub

--- 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.py
+++ 3093-longest-common-suffix-queries/3093-longest-common-suffix-queries.py
@@ -3,6 +3,15 @@ class Solution:
         trie = {
             'idx': 0
         }
+        def is_better(new: int, old:int) -> bool: 
+            new_len = len(wordsContainer[new])
+            old_len = len(wordsContainer[old])
+            if new_len < old_len:
+                return True
+            if new_len == old_len and new < old:
+                return True
+            return False
+
         def link_trie(index: int, dict_pointer: dict, rest_word: str):
             if len(rest_word) == 0:
                 return
@@ -12,19 +21,13 @@ class Solution:
                     'idx': index
                 }
             idx = dict_pointer[cur_char]['idx']
-            if (
-                (len(wordsContainer[idx]) > len(wordsContainer[index]))
-                or (len(wordsContainer[idx]) == len(wordsContainer[index]) and idx > index)
-            ):
+            if (is_better(index, idx)):
                 dict_pointer[cur_char]['idx'] = index
             link_trie(index, dict_pointer[cur_char], rest_word[:-1])
         
         for i in range(len(wordsContainer)):
             idx = trie['idx']
-            if (
-                (len(wordsContainer[idx]) > len(wordsContainer[i]))
-                or (len(wordsContainer[idx]) == len(wordsContainer[i]) and idx > i)
-            ):
+            if (is_better(i, idx)):
                 trie['idx'] = i
             link_trie(i, trie, wordsContainer[i])
 
