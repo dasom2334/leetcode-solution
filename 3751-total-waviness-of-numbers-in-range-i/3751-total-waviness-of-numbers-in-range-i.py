dbd8c5e172908249a75b731b5df0a2e92d68e537
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2026-06-04 14:31:25 +0900

    Time: 377 ms (29.12%), Space: 20.1 MB (8.05%) - LeetHub

---
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.py
@@ -0,0 +1,30 @@
+class Solution:
+    def totalWaviness(self, num1: int, num2: int) -> int:
+        calc_map = {}
+        def calc_waviness(num: int):
+            if num > 1000:
+                raise ValueError()
+            num1 = (num // 100) * 100
+            num2 = ((num - num1) // 10) * 10
+            num3 = num - num1 - num2
+            num1 //= 100
+            num2 //= 10
+            peak = num1 < num2 > num3
+            valley = num1 > num2 < num3
+            return 1 if peak or valley else 0
+
+        def get_waviness(num: int):
+            if num < 100:
+                return 0
+            if num in calc_map:
+                calc_map[num]
+            if num < 1000:
+                return calc_waviness(num)
+            pre = get_waviness(num // 10)
+            cur = calc_waviness(num % 1000)
+            return pre + cur
+        
+        waviness = 0
+        for i in range(num1, num2 + 1):
+            waviness += get_waviness(i)
+        return waviness
