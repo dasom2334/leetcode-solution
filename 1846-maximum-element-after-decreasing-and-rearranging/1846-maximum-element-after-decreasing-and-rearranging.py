2207c148ce0451de7db2265943f1144819d1a789
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2026-06-29 01:00:34 +0900

    Time: 14 ms (91.49%), Space: 28 MB (98.58%) - LeetHub

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -1,9 +1,6 @@
 class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
         sorted_arr = sorted(arr)
-        n = len(arr)
-        print(sorted_arr)
-
         cur = 0
         for num in sorted_arr:
             cur = min(cur + 1, num)
