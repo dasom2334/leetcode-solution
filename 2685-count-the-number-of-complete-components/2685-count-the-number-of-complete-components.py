c9b21a581b9b84dc7771e9284ee9908bc8489ca9
dasom2334 <75345414+dasom2334@users.noreply.github.com>
2026-07-11 19:34:40 +0900

    Time: 45 ms (50.47%), Space: 19.7 MB (71.89%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.py
@@ -1,25 +1,24 @@
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
-        cnts = [0] * n
-        mem = defaultdict(list)
+        mem = defaultdict(set)
+        for i in range(n):
+            mem[i].add(i)
+
         for i, j in edges:
             if i > j:
                 i, j = j, i
-            mem[i].append(j)
-            mem[j].append(i)
-            cnts[i] += 1
-            cnts[j] += 1
+            mem[i].add(j)
+            mem[j].add(i)
         visited = [False] * n
         result = 0
         for i in range(n):
             if visited[i]:
                 continue
             cnt = len(mem[i])
-            complete = not visited[i] or cnts[i] != cnt
             visited[i] = True
-            s = set(mem[i] + [i])
+            complete = True
             for m in mem[i]:
-                if len(mem[m]) != cnt or visited[m] or s != set(mem[m] + [m]):
+                if mem[i] != mem[m]:
                     complete = False
                 visited[m] = True
             if complete:
